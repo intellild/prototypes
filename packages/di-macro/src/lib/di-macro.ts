b149c4ad206d0db8762b4fd4b52da207626d320b
intellild <intellild@outlook.com>
2022-02-23 17:01:22 +0800

    injectable macro

--- packages/di-macro/src/lib/di-macro.ts
+++ packages/di-macro/src/lib/di-macro.ts
@@ -1,3 +1,90 @@
-export function diMacro(): string {
-  return 'di-macro';
+import { createMacro } from 'babel-plugin-macros';
+import * as t from '@babel/types';
+import { serializeType } from './serializeTypeReference';
+
+type TokenNode =
+  | t.Identifier
+  | t.UnaryExpression
+  | t.ConditionalExpression
+  | t.Expression;
+
+function createFactory(tokens: TokenNode[], klassName: string) {
+  const args = tokens.map((token) =>
+    t.callExpression(
+      t.memberExpression(t.identifier('container'), t.identifier('resolve')),
+      [token]
+    )
+  );
+
+  return t.classMethod(
+    'method',
+    t.identifier('factory'),
+    [t.identifier('container'), t.identifier('context')],
+    t.blockStatement([
+      t.returnStatement(t.newExpression(t.identifier(klassName), args)),
+    ]),
+    false,
+    true
+  );
 }
+
+export const injectable = createMacro(({ references }) => {
+  for (const reference of references.default) {
+    const decorator = reference.findParent((parentPath) =>
+      parentPath.isDecorator()
+    );
+    if (!decorator) {
+      throw new Error();
+    }
+    const klass = decorator.parentPath;
+    if (!klass) {
+      throw new Error();
+    }
+    if (!klass.isClassDeclaration()) {
+      throw new Error();
+    }
+    if (!klass.node.id) {
+      throw new Error();
+    }
+    klass.traverse({
+      ClassMethod(method) {
+        if (method.node.kind !== 'constructor') {
+          return;
+        }
+        const tokens: TokenNode[] = [];
+        for (const param of method.node.params) {
+          if (param.type === 'RestElement') {
+            throw new Error();
+          }
+          const reservedDecorator: t.Decorator[] = [];
+          let resolvedByDecorator = false;
+          param.decorators?.forEach((paramDecorator) => {
+            if (paramDecorator) {
+              resolvedByDecorator = true;
+              if (paramDecorator.expression.type !== 'CallExpression') {
+                throw new Error();
+              }
+              if (!paramDecorator.expression.arguments.length) {
+                throw new Error();
+              }
+              const argument = paramDecorator.expression.arguments[0];
+              if (!t.isExpression(argument)) {
+                throw new Error();
+              }
+              tokens.push(argument);
+            } else {
+              reservedDecorator.push(paramDecorator);
+            }
+          });
+          param.decorators = reservedDecorator;
+          if (resolvedByDecorator) {
+            continue;
+          }
+          const serialized = serializeType(klass, param);
+          tokens.push(serialized);
+        }
+        klass.node.body.body.push(createFactory(tokens, klass.node.id.name));
+      },
+    });
+  }
+});
