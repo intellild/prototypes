d690135fdb3b6a270c1ce9aa2dcb3ca891a1fd23
intellild <intellild@outlook.com>
2023-03-06 11:33:20 +0800

    upgrade nx

--- packages/di-macro/jest.config.ts
+++ packages/di-macro/jest.config.ts
@@ -2,14 +2,15 @@
 export default {
   displayName: 'di-macro',
   preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  globals: {},
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[t]sx?$': 'ts-jest',
+    '^.+\\.[t]sx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/packages/di-macro',
